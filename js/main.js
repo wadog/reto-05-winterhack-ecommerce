136322b061774ad03c7cdc7225cb2448bb32626a
walter <walthermaximo@gmail.com>
2023-04-25 04:20:24 -0300

    arreglo funcion ver categorias, falta popup y css

--- js/main.js
+++ js/main.js
@@ -11,33 +11,18 @@ const categoriaProductos = document.querySelectorAll('.categoria_productos')
 const contador = 0
 let productos = []
 let carritoCompra = []
-
+console.log(categoriaProductos)
 fetch('https://fakestoreapi.com/products/')
-.then(res => res.json())
-.then(json => {
-    productos = Object.values(json)
-    console.log(productos, 'los productos extraidos')
-    productoRandom()
-})
-.catch(error => console.error(error))
-
-//fetch('https://fakestoreapi.com/products/')
-//    .then(res => {
-//        if (!res.ok) {
-//            throw new Error(`Error: ${res.status} - ${res.statusText}`);
-//        }
-//        return res.json();
-//    })
-//    .then(productos => {
-//        console.log(productos, 'los productos extraidos');
-//        productoRandom(productos);
-//    })
-//    .catch(error => {
-//        console.error(error);
-//    });
+    .then(res => res.json())
+    .then(json => {
+        productos = Object.values(json)
+        console.log(productos, 'los productos extraidos')
+        productoRandom()
+    })
+    .catch(error => console.error(error))
 
 /* boton home */
-let volverHome = () => {
+const volverHome = () => {
     console.log('volviendo al inicio')
     home.classList.remove('dNone')
     lista.classList.add('dNone')
@@ -101,129 +86,73 @@ const productoRandom = () => {
     })
 }
 
-document.addEventListener('click', (event) => {
-    let categoria = event.target.id
-    productos.forEach((prod) => {
-        if (prod.category === categoria) {
-            console.log(categoria);
-            lista.innerHTML = ''
-            lista.classList.remove('dNone')
-            home.classList.add('dNone')
-            console.log(`ver lista de productos ${categoria}`)
-            lista.innerHTML += `
+categoriaProductos.forEach(categoria => {
+    categoria.addEventListener('click', (event) => {
+        const categoriaSeleccionada = event.target.className;
+            console.log(categoriaSeleccionada)
+        const productosFiltrados = productos.filter(producto => producto.category === categoriaSeleccionada)
+            console.log(productosFiltrados)
+        lista.innerHTML = '';
+        productosFiltrados.forEach(producto => {
+            console.log(producto)
+            const productoHTML = `
                 <div class="card" style="width: 18rem;">
-                    <img src="${prod.image}" 
-                    class="card-img-top" 
-                    alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">${prod.title}</h5>
-                        <p class="card-text">${prod.category}</p>
-                        <button type="button" 
-                            class="btn btn-primary" 
-                            data-bs-toggle="modal" 
-                            data-bs-target="#exampleModal">
-                            ver mas
-                        </button>
-                    </div>
+                    <img src="${producto.image}"
+                        class="card-img-top"
+                        alt="${producto.title}">
+                <div class="card-body">
+                    <h3>${producto.title}</h3>
+                    <p>${producto.price}</p>
                 </div>
-                `
-        }
+                <button type="button"
+                    class="btn btn-primary"
+                    data-bs-toggle="modal"
+                    data-bs-target="#exampleModal">
+                    ver mas
+                </button>
+        </div>
+        `
+            lista.innerHTML += productoHTML
+        })
+        home.classList.add('dNone');
+        lista.classList.remove('dNone');
     })
 })
 
-/*     mostrando la lista de cada*/
-
-// let mujer = () => {
-//     lista.innerHTML = ''
-//     lista.classList.remove('dNone')
-//     home.classList.add('dNone')
-//     console.log('ver lista de productos')
-//     productos.forEach((prod) => {
-//         if (prod.category == "women's clothing") {
-//             lista.innerHTML += `
-//             <div class="card" style="width: 18rem;">
-//                 <img src="${prod.image}" 
-//                     class="card-img-top"
-//                     alt="...">
-//             <div class="card-body">
-//                 <h5 class="card-title">${prod.title}</h5>
-//                 <p class="card-text">${prod.category}</p>
-//                 <button type="button" 
-//                 class="btn btn-primary" 
-//                 data-bs-toggle="modal" 
-//                 data-bs-target="#exampleModal">
-//                 ver mas
-//                 </button>
-//             </div>
-//             </div>
-//             `
-//         }
-//     })
-// }
-// let joyeria = () => {
-//     lista.innerHTML = ''
-//     lista.classList.remove('dNone')
-//     home.classList.add('dNone')
-//     console.log('ver lista de productos')
-//     productos.forEach((prod) => {
-//         if (prod.category == "jewelery") {
-//             lista.innerHTML += `
-//                 <div class="card" style="width: 18rem;">
-//                     <img src="${prod.image}" 
-//                     class="card-img-top" 
-//                     alt="...">
-//                 <div class="card-body">
-//                     <h5 class="card-title">${prod.title}</h5>
-//                     <p class="card-text">${prod.category}</p>
-//                     <button type="button" 
-//                     class="btn btn-primary" 
-//                     data-bs-toggle="modal" 
-//                     data-bs-target="#exampleModal">
-//                     ver mas
-//                     </button>
-//                 </div>
-//                 </div>
-//                 `
-//         }
-//     })
-// }
-// let electro = () => {
-//     lista.innerHTML = ''
-//     lista.classList.remove('dNone')
-//     home.classList.add('dNone')
-//     console.log('ver lista de productos')
-//     productos.forEach((prod) => {
-//         if (prod.category == "electronics") {
-//             lista.innerHTML += `
-//                 <div class="card" style="width: 18rem;">
-//                     <img src="${prod.image}" 
-//                     class="card-img-top" 
-//                     alt="...">
-//                 <div class="card-body">
-//                     <h5 class="card-title">${prod.title}</h5>
-//                     <p class="card-text">${prod.category}</p>
-//                     <button type="button" 
-//                         class="btn btn-primary verMas" 
-//                         data-bs-toggle="modal" 
-//                         data-bs-target="#exampleModal">
-//                         ver mas
-//                     </button>
-//                 </div>
-//                 </div>
-//                 `
-//         }
-//     })
-// }
-// /* agregando productos al carrito */
-// let verCarrito = (e) => {
-//     alert('iendo al acarrito')
-// }
-
-
-
-// const productDescription = (productoRandom, index) => {
-//     const { image, title, description, price } = productoRandom[index]
-//     console.log('ver mas')
+//document.addEventListener('click', (event) => {
+//    let categoria = event.target.classList
+//    productos.forEach((prod) => {
+//        if (prod.category === categoria) {
+//            console.log(categoria);
+//            lista.innerHTML = ''
+//            lista.classList.remove('dNone')
+//            home.classList.add('dNone')
+//            console.log(`ver lista de productos ${categoria}`)
+//            lista.innerHTML += `
+//                        <div class="card" style="width: 18rem;">
+//                            <img src="${prod.image}"
+//                            class="card-img-top"
+//                            alt="...">
+//                            <div class="card-body">
+//                                <h5 class="card-title">${prod.title}</h5>
+//                                <p class="card-text">${prod.category}</p>
+//                                <button type="button"
+//                                    class="btn btn-primary"
+//                                    data-bs-toggle="modal"
+//                                    data-bs-target="#exampleModal">
+//                                    ver mas
+//                                </button>
+//                            </div>
+//                        </div>
+//                        `
+//        }
+//    })
+//})
 //
+
+/* agregando productos al carrito */
+let verCarrito = () => {
+    alert('iendo al acarrito')
+}
 
 
