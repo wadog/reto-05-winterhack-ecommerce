70201b409dd540d0304f8c35ad3b19a2f7f53577
walter <walthermaximo@gmail.com>
2023-05-01 06:49:58 -0300

    arreglo de modal, falta carrito y css

--- js/main.js
+++ js/main.js
@@ -6,11 +6,13 @@ const lista = document.querySelector('.lista')
 const carrito = document.querySelector('.carrito')
 const carritoHeader = document.querySelector('#carritoHeader')
 const contadorProductos = document.querySelector('.contadorProductos')
-const modal = document.querySelector('.modal-content')
+const modal = document.querySelector('.modal-body')
 const categoriaProductos = document.querySelectorAll('.categoria_productos')
-const contador = 0
+let contador = 0
 let productos = []
 let carritoCompra = []
+
+
 
 fetch('https://fakestoreapi.com/products/')
     .then(res => res.json())
@@ -23,7 +25,6 @@ fetch('https://fakestoreapi.com/products/')
 
 /* boton home */
 const volverHome = () => {
-    console.log('volviendo al inicio')
     home.classList.remove('dNone')
     lista.classList.add('dNone')
     productoRandom()
@@ -35,7 +36,6 @@ const cargarPagina = () => {
 }
 
 const inicio = setTimeout(cargarPagina, 2000)
-
 const productoRandom = () => {
     const random = Math.floor(Math.random() * productos.length);
     const productRandom = productos[random];
@@ -46,77 +46,84 @@ const productoRandom = () => {
             </div>
             <div class="col-6">
             <h1>${productRandom.title}</h1>
-            <button type="button" id="btnVerMas"
-            class="btn btn-warning " 
+            <button type="button"
+            class="btn btn-warning btnVerMas " 
             data-bs-toggle="modal" 
             data-bs-target="#exampleModal">
                     ver mas
                 </button>
             </div>
-        </div>`
-    const btnVerMas = document.querySelector('#btnVerMas');
-    btnVerMas.addEventListener('click', () => {
-        console.log('Soy un btn recien nacido')
-        modal.innerHTML = '';
-        modal.innerHTML += `
-            <div class="modal-body">
-            <button type="button" 
+        </div>
+        `
+    modal.innerHTML = ''
+    modal.innerHTML += `
+        <button type="button" 
             class="btn-close" 
-            data-bs-dismiss="modal" 
-                    aria-label="Close">
-                </button>    
-                <img src="${productRandom.image}" alt="">
-                <h5 class="modal-title" 
-                    id="exampleModalLabel">
-                        ${productRandom.title}
-                </h5>
-                <p>${productRandom.description}</p>
-                <p>$ ${productRandom.price}</p>
-                <button type="button" id="comprar" 
-                class="btn btn-primary">
-                comprar
-                </button>
-            </div>`
-        const btnComprar = document.querySelector('#comprar')
-        console.log(btnComprar)
-        btnComprar.addEventListener('click', () => {
-            contador++
-            contadorProductos.innerHTML = contador
-        })
-    })
+                data-bs-dismiss="modal" 
+                aria-label="Close">
+        </button>    
+        <img src="${productRandom.image}" alt="">
+        <h5 class="modal-title" 
+            id="exampleModalLabel">
+                ${productRandom.title}
+        </h5>
+        <p>${productRandom.description}</p>
+        <p>$ ${productRandom.price}</p>
+        `
 }
 
 categoriaProductos.forEach(categoria => {
     categoria.addEventListener('click', (event) => {
         const categoriaSeleccionada = event.target.className;
-            //console.log(categoriaSeleccionada)
         const productosFiltrados = productos.filter(producto => producto.category === categoriaSeleccionada)
-            //console.log(productosFiltrados)
         lista.innerHTML = '';
         productosFiltrados.forEach(producto => {
-            //console.log(producto)
             const productoHTML = `
-                <div class="card" style="width: 18rem;">
-                    <img src="${producto.image}"
-                        class="card-img-top"
-                        alt="${producto.title}">
-                <div class="card-body">
+            <div class="card" style="width: 18rem;">
+            <img src="${producto.image}"
+            class="card-img-top"
+            alt="${producto.title}">
+            <div class="card-body">
                     <h3>${producto.title}</h3>
                     <p>${producto.price}</p>
                 </div>
                 <button type="button"
-                    class="btn btn-primary"
+                    class="btn btn-primary ver-mas"
                     data-bs-toggle="modal"
-                    data-bs-target="#exampleModal">
+                    data-bs-target="#exampleModal"
+                    data-id="${producto.id}">
                     ver mas
                 </button>
-        </div>
-        `
+                <button type="button" id="comprar" class="btn btn-primary">
+                agregar al carrito
+            </button>
+        </div>`
             lista.innerHTML += productoHTML
+            modal.innerHTML = ''
+            const botonesVerMas = document.querySelectorAll('.ver-mas');
+            botonesVerMas.forEach(boton => {
+                boton.addEventListener('click', (event) => {
+                    const productoId = parseInt(event.target.getAttribute('data-id'));
+                    const productoSeleccionado = productos.find(producto => producto.id === productoId);
+                    modal.innerHTML = `
+            <img src="${productoSeleccionado.image}" alt="">
+            <h5 class="modal-title" id="exampleModalLabel">${productoSeleccionado.title}</h5>
+            <p>${productoSeleccionado.description}</p>
+            <p>$ ${productoSeleccionado.price}</p>`;
+                });
+            });
         })
         home.classList.add('dNone');
         lista.classList.remove('dNone');
+
     })
+})
+
+let btnComprar = document.querySelector('#comprar')
+btnComprar.addEventListener('click', () => {
+    contador++
+    contadorProductos.innerHTML = contador
+    console.log(contador)
 })
 
 /* agregando productos al carrito */
